d39469bc7c2c9ab97d695005cd35871b10f48de2
Oreeeee <oree3ee@gmail.com>
2024-10-09 23:25:36 +0200

    Add an initial user to init.sql

--- init.sql
+++ init.sql
@@ -1,5 +1,14 @@
+-- Schema
 CREATE TABLE gguser (
     uin SERIAL PRIMARY KEY,
     password_gg32 BIGINT,
     password_sha1 VARCHAR(20)
 );
+
+-- Initial data
+
+-- Creates an initial user with the number 1 and password 123
+INSERT INTO gguser
+    (uin, password_gg32, password_sha1)
+    VALUES
+    (1, 4105424095, NULL);
