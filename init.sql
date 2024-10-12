3a28f595666d1b8f2c8012540a55a8e4670e2ec6
Oreeeee <oree3ee@gmail.com>
2024-10-13 01:59:40 +0200

    Add more varied ads to init.sql

--- init.sql
+++ init.sql
@@ -22,6 +22,46 @@ INSERT INTO gguser
     (1, 4105424095, NULL); -- Creates an initial user with the number 1 and password 123
 
 INSERT INTO adserver_ad
+(adtype, bannertype, html)
+VALUES
+    (0, 0, 'Hello from poggadaj-HTTP!');
+
+INSERT INTO adserver_ad
+(adtype, bannertype, html)
+VALUES
+    (0, 0, 'This is one of the banner responses.');
+
+INSERT INTO adserver_ad
+(adtype, bannertype, html)
+VALUES
+    (0, 0, 'Lorem ipsum');
+
+INSERT INTO adserver_ad
+(adtype, bannertype, html)
+VALUES
+    (0, 1, 'Hello from poggadaj-HTTP!');
+
+INSERT INTO adserver_ad
+(adtype, bannertype, html)
+VALUES
+    (0, 1, 'This is one of the small banner responses.');
+
+INSERT INTO adserver_ad
+(adtype, bannertype, html)
+VALUES
+    (0, 1, 'Lorem ipsum');
+
+INSERT INTO adserver_ad
     (adtype, bannertype, html)
     VALUES
-    (0, 3, '<h1>Hello from poggadaj-HTTP!</h1>');
+    (0, 2, 'Hello from poggadaj-HTTP!');
+
+INSERT INTO adserver_ad
+(adtype, bannertype, html)
+VALUES
+    (0, 2, 'This is one of the main banner responses.');
+
+INSERT INTO adserver_ad
+(adtype, bannertype, html)
+VALUES
+    (0, 2, 'Lorem ipsum');
