b94c84539bd11113445c4e151d20af57f58bc7bb
Oreeeee <oree3ee@gmail.com>
2024-10-13 00:34:14 +0200

    Add a better logging solution

--- poggadaj-tcp/GG60_ProtocolHandler.go
+++ poggadaj-tcp/GG60_ProtocolHandler.go
@@ -14,7 +14,7 @@ func MsgChannel_GG60(currConn *GGConnection) {
 	for {
 		msg := RecvMessageChannel(pubsub)
 
-		fmt.Println(currConn.UIN, " received a message!")
+		Logger.Debugf("%d received a message!", currConn.UIN)
 		pS := universal.GG_Recv_MSG{
 			Sender:   msg.From,
 			Seq:      0,
@@ -25,7 +25,7 @@ func MsgChannel_GG60(currConn *GGConnection) {
 		pOut := universal.InitGG_Packet(universal.GG_RECV_MSG, pS.Serialize())
 		_, err := pOut.Send(currConn.Conn)
 		if err != nil {
-			fmt.Println("Error: ", err)
+			Logger.Errorf("Error: %s", err)
 		}
 	}
 }
@@ -34,16 +34,16 @@ func StatusChannel_GG60(currConn *GGConnection) {
 	pubsub := GetStatusChannel()
 	for {
 		statusChange := RecvStatusChannel(pubsub)
-		fmt.Println(statusChange)
+		//fmt.Println(statusChange)
 
 		// Check if the status change is applicable for this connection
 		for _, e := range currConn.NotifyList {
 			if e.UIN == statusChange.UIN {
-				fmt.Printf("%d's status change is relevant for %d\n", statusChange.UIN, currConn.UIN)
+				Logger.Debugf("%d's status change is relevant for %d\n", statusChange.UIN, currConn.UIN)
 
 				status := statusChange.Status
 				if status == universal.GG_STATUS_INVISIBLE {
-					fmt.Println("Got GG_STATUS_INVISIBLE, sending GG_STATUS_NOT_AVAIL")
+					Logger.Debugf("Got GG_STATUS_INVISIBLE, sending GG_STATUS_NOT_AVAIL")
 					status = universal.GG_STATUS_NOT_AVAIL
 				}
 
@@ -59,7 +59,7 @@ func StatusChannel_GG60(currConn *GGConnection) {
 				pOut := universal.InitGG_Packet(universal.GG_STATUS60, p.Serialize())
 				_, err := pOut.Send(currConn.Conn)
 				if err != nil {
-					fmt.Println("Error: ", err)
+					Logger.Errorf("Error: %s", err)
 				}
 			}
 		}
@@ -70,17 +70,16 @@ func Handle_GG60(currConn GGConnection, pRecv universal.GG_Packet) {
 	// Handle the initial log in
 	p := gg60.GG_Login60{}
 	p.Deserialize(pRecv.Data)
-	fmt.Println("Decoded data: ", p)
 
 	currConn.UIN = p.UIN
 
-	fmt.Println("Sending login response")
+	Logger.Debugf("Sending login response")
 	passHash, _ := GetGG32Hash(currConn.UIN)
 	if p.Hash == passHash {
 		currConn.Authenticated = true
 		currConn.Status = p.Status
 
-		fmt.Println("Sending GG_LOGIN_OK")
+		Logger.Debugf("Sending GG_LOGIN_OK")
 		pOut := universal.InitGG_Packet(universal.GG_LOGIN_OK, []byte{})
 		_, err := pOut.Send(currConn.Conn)
 		if err != nil {
@@ -93,7 +92,7 @@ func Handle_GG60(currConn GGConnection, pRecv universal.GG_Packet) {
 		// Change user's status to not available after disconnect
 		defer SetUserStatus(currConn.UIN, universal.GG_STATUS_NOT_AVAIL)
 	} else {
-		fmt.Println("Sending GG_LOGIN_FAILED")
+		Logger.Debugf("Sending GG_LOGIN_FAILED")
 		pOut := universal.InitGG_Packet(universal.GG_LOGIN_FAILED, []byte{})
 		_, err := pOut.Send(currConn.Conn)
 		if err != nil {
@@ -111,18 +110,18 @@ func Handle_GG60(currConn GGConnection, pRecv universal.GG_Packet) {
 		pRecv := universal.GG_Packet{}
 		err := pRecv.Receive(currConn.Conn)
 		if err != nil {
-			fmt.Println("Error receiving data, dropping connection!: ", err)
+			Logger.Errorf("Error receiving data, dropping connection!: %s", err)
 			return
 		}
 
 		switch pRecv.PacketType {
 		case universal.GG_NOTIFY_FIRST:
-			fmt.Println("Received GG_NOTIFY_FIRST")
+			Logger.Debugf("Received GG_NOTIFY_FIRST")
 			universal.GG_NotifyContactDeserialize(pRecv.Data, pRecv.Length, &currConn.NotifyList)
 		case universal.GG_NOTIFY_LAST:
-			fmt.Println("Received GG_NOTIFY_LAST")
+			Logger.Debugf("Received GG_NOTIFY_LAST")
 			universal.GG_NotifyContactDeserialize(pRecv.Data, pRecv.Length, &currConn.NotifyList)
-			fmt.Println(currConn.NotifyList)
+			//fmt.Println(currConn.NotifyList)
 
 			// Respond with GG_NOTIFY_REPLY
 			response := make([]byte, 0)
@@ -138,43 +137,43 @@ func Handle_GG60(currConn GGConnection, pRecv universal.GG_Packet) {
 			pOut := universal.InitGG_Packet(universal.GG_NOTIFY_REPLY60, buf.Bytes())
 			_, err := pOut.Send(currConn.Conn)
 			if err != nil {
-				fmt.Println("Error: ", err)
+				Logger.Debugf("Error: %s", err)
 			}
 		case universal.GG_ADD_NOTIFY:
-			fmt.Println("Received GG_ADD_NOTIFY")
+			Logger.Debugf("Received GG_ADD_NOTIFY")
 			universal.GG_AddNotify(pRecv.Data, &currConn.NotifyList)
-			fmt.Println(currConn.NotifyList)
+			//fmt.Println(currConn.NotifyList)
 		case universal.GG_REMOVE_NOTIFY:
-			fmt.Println("Received GG_REMOVE_NOTIFY (unimplemented)")
+			Logger.Debugf("Received GG_REMOVE_NOTIFY (unimplemented)")
 		case universal.GG_LIST_EMPTY:
-			fmt.Println("Received GG_LIST_EMPTY")
+			Logger.Debugf("Received GG_LIST_EMPTY")
 		case universal.GG_NEW_STATUS:
-			fmt.Println("Received GG_NEW_STATUS")
+			Logger.Debugf("Received GG_NEW_STATUS")
 
 			p := universal.GG_New_Status{}
 			p.Deserialize(pRecv.Data)
 
 			SetUserStatus(currConn.UIN, p.Status)
 
-			fmt.Println("New status: ", p.Status)
+			Logger.Debugf("New status: %d", p.Status)
 		case universal.GG_SEND_MSG:
-			fmt.Println("Client is sending a message...")
+			Logger.Debugf("Client is sending a message...")
 
 			p := universal.GG_Send_MSG{}
 			p.Deserialize(pRecv.Data, pRecv.Length)
-			fmt.Printf("Recipient: %d, Message: %s\n", p.Recipient, p.Content)
+			//fmt.Printf("Recipient: %d, Message: %s\n", p.Recipient, p.Content)
 
 			PublishMessageChannel(p.Recipient, Message{currConn.UIN, p.Content})
 		case universal.GG_PING:
-			fmt.Println("Received GG_PING")
+			Logger.Debugf("Received GG_PING")
 
 			pOut := universal.InitGG_Packet(universal.GG_PONG, []byte{})
 			_, err := pOut.Send(currConn.Conn)
 			if err != nil {
-				fmt.Println("Error: ", err)
+				Logger.Errorf("Error: %s", err)
 			}
 		default:
-			fmt.Printf("Received unknown packet, ignoring: 0x00%x\n", pRecv.PacketType)
+			Logger.Warnf("Received unknown packet, ignoring: 0x00%x\n", pRecv.PacketType)
 		}
 	}
 }
--- poggadaj-tcp/main.go
+++ poggadaj-tcp/main.go
@@ -2,17 +2,19 @@ package main
 
 import (
 	"fmt"
+	"github.com/charmbracelet/log"
 	"github.com/jackc/pgx/v5/pgxpool"
 	"github.com/redis/go-redis/v9"
-	"log"
 	"net"
 	"os"
 	"poggadaj-tcp/gg60"
 	"poggadaj-tcp/universal"
+	"time"
 )
 
 var DatabaseConn *pgxpool.Pool
 var CacheConn *redis.Client
+var Logger *log.Logger
 
 // This function handles connections before the client version of GG is known.
 // Its purpose is to send GG_WELCOME, receive the packet type of the incoming
@@ -27,19 +29,18 @@ func handleConnection(currConn GGConnection) {
 
 	_, err := packet.Send(currConn.Conn)
 	if err != nil {
-		fmt.Println("Error: ", err)
+		Logger.Errorf("Error: %s", err)
 	}
-	fmt.Println("Sent data")
 
 	// Wait for the next packet, which will tell us the protocol version handler we need
 	pRecv := universal.GG_Packet{}
 	if pRecv.Receive(currConn.Conn) != nil {
-		fmt.Println("Error receiving data, dropping connection!: ", err)
+		Logger.Errorf("Error receiving data, dropping connection!: %s", err)
 		return
 	}
 
 	if pRecv.PacketType == gg60.GG_LOGIN60 {
-		fmt.Println("Received GG_LOGIN60, switching to Handle_GG60")
+		Logger.Infof("Gadu-Gadu 6.0 protocol detected")
 		Handle_GG60(currConn, pRecv)
 	}
 }
@@ -50,26 +51,33 @@ func main() {
 
 	CacheConn = GetCacheConn()
 
+	Logger = log.NewWithOptions(os.Stdout, log.Options{
+		ReportCaller:    true,
+		ReportTimestamp: true,
+		TimeFormat:      time.DateTime,
+		Level:           log.DebugLevel,
+	})
+
 	l, err := net.Listen("tcp", fmt.Sprintf("%s:8074", os.Getenv("LISTEN_ADDRESS")))
 	if err != nil {
-		log.Fatal(err)
+		Logger.Fatal(err)
 		return
 	}
 	defer l.Close()
 	defer DatabaseConn.Close()
 
-	fmt.Println("Listening...")
+	Logger.Infof("Listening on %s:%d", os.Getenv("LISTEN_ADDRESS"), 8074)
 
 	var connList []*GGConnection
 
 	for {
 		conn, err := l.Accept()
 		if err != nil {
-			fmt.Println("Error accepting: ", err)
+			Logger.Errorf("Error accepting from %s: %s", conn.RemoteAddr(), err)
 			continue
 		}
 
-		fmt.Println("Accepted connection: ", conn.RemoteAddr())
+		Logger.Infof("Accepted connection from %s", conn.RemoteAddr())
 
 		// Create a connection object
 		ggConn := &GGConnection{}
