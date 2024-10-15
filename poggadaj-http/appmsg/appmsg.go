ea90765c8e6c9c7617266f493272529eae571f9d
Oreeeee <oree3ee@gmail.com>
2024-10-15 21:03:37 +0200

    Allow the configuration of values returned by the /appsvc/appmsg4.asp endpoint

--- poggadaj-http/appmsg/appmsg.go
+++ poggadaj-http/appmsg/appmsg.go
@@ -1,11 +1,14 @@
 package appmsg
 
 import (
+	"fmt"
 	"github.com/gin-gonic/gin"
 	"net/http"
+	"os"
 )
 
 func AppMSG_Handler(c *gin.Context) {
-	// TODO: Dynamically return the IP addresses
-	c.String(http.StatusOK, "26679 0 127.0.0.1:8074 127.0.0.1")
+	ip := os.Getenv("GG_SERVICE_IP")
+	port := os.Getenv("GG_SERVICE_PORT")
+	c.String(http.StatusOK, fmt.Sprintf("0 0 %s:%s %s", ip, port, ip))
 }
