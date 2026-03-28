e05798a04b0476a229b3bd2f0b365e54812a572e
Oreeeee <oree3ee@gmail.com>
2026-03-28 23:16:46 +0100

    Add HTML header

--- poggadaj-frontend/go.mod
+++ poggadaj-frontend/go.mod
@@ -5,3 +5,5 @@ replace poggadaj-shared => ./shared
 go 1.26
 
 require poggadaj-shared v0.0.0
+
+require github.com/labstack/echo/v5 v5.0.4 // indirect
--- poggadaj-frontend/main.go
+++ poggadaj-frontend/main.go
@@ -1,12 +1,33 @@
 package main
 
 import (
-	"fmt"
+	"html/template"
+	"io"
+	"net/http"
 	_ "poggadaj-shared"
-	"time"
+
+	"github.com/labstack/echo/v5"
 )
 
+type TemplateRenderer struct {
+	templates *template.Template
+}
+
+func (t *TemplateRenderer) Render(c *echo.Context, w io.Writer, name string, data any) error {
+	return t.templates.ExecuteTemplate(w, name, data)
+}
+
 func main() {
-	fmt.Println("Hello World")
-	time.Sleep(time.Hour)
+	e := echo.New()
+	e.Renderer = &TemplateRenderer{
+		templates: template.Must(template.ParseGlob("html/*.html")),
+	}
+
+	e.GET("/", func(c *echo.Context) error {
+		return c.Render(http.StatusOK, "index.html", nil)
+	})
+
+	if err := e.Start(":3000"); err != nil {
+		e.Logger.Error("shutting down the server", "error", err)
+	}
 }
